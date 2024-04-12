721872da3accc7c8d4d9d95dd580a674a33bac88
atlasmoth <ebuka422@gmail.com>
2024-04-12 18:51:20 +0100

    quick fix

--- src/index.tsx
+++ src/index.tsx
@@ -58,7 +58,7 @@ app.frame("/", async (c) => {
         }
         const buttons: any[] = [];
         if (willRedirect) {
-          buttons.push(<Button.Link href={willRedirect}>Visit ↗</Button.Link>);
+          buttons.push(<Button.Link href={willRedirect}>Visit</Button.Link>);
         }
         buttons.push(<Button.Reset>Reset</Button.Reset>);
         return c.res(infoScreen(returnedText, buttons));
