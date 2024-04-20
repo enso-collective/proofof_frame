51cc913eb1068e558e532915caf64fd7dbd7b4c1
atlasmoth <ebuka422@gmail.com>
2024-04-20 22:36:13 +0100

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -91,12 +91,14 @@ app.frame("/payments/:validationId", async (c) => {
     const tx = await provider.getTransaction(
       c.transactionId || c.buttonValue || `0x`
     );
-
+    console.log({ buttonId: c.buttonValue });
+    console.log({ transactionId: c.transactionId });
+    console.log(tx);
     if (!tx) {
       const buttons = [<Button value={c.transactionId}>Check progress</Button>];
       const returnObj = {
         ...infoScreen("Completing transaction...", buttons),
-        action: `/payments/${c.transactionId}`,
+        action: `/payments/${c.transactionId || c.buttonValue}`,
       };
       return c.res(returnObj);
     }
