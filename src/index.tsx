18953c4fc5f25856d1a8142ba8c0b2c559332bf8
atlasmoth <ebuka422@gmail.com>
2024-04-21 20:09:37 +0100

    testing stuff

--- src/index.tsx
+++ src/index.tsx
@@ -74,7 +74,7 @@ app.frame("/", async (c) => {
                 src={embedWithImage.url}
                 objectFit="contain"
                 width={"256"}
-                height={"128"}
+                height={"256"}
               />
             ),
           };
