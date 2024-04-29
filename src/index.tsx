742d0f771b3c5613f66f49caf7f3ea8ba1471110
atlasmoth <ebuka422@gmail.com>
2024-04-29 17:25:41 +0100

    testing new aspect ratio

--- src/index.tsx
+++ src/index.tsx
@@ -74,7 +74,7 @@ app.frame("/", async (c) => {
             image: (
               <Image
                 src={embedWithImage.url}
-                objectFit="contain"
+                objectFit="cover"
                 width={"224"}
                 height={"224"}
               />
